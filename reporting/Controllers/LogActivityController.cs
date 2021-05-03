f700d0bb2488bdc667986dd818712fe08f04ea3b
billy.stack <billy.stack@channeladvisor.com>
2021-05-03 22:19:55 +0100

    Added reporting microservice, this is still a work in progress (unsure of get controller execution)

---
+++ reporting/Controllers/LogActivityController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using reporting.Models;
+using System;
+using System.Linq;
+
+namespace reporting.Controllers
+{
+    [Route("api/[controller]")]
+    public class LogActivityController : ControllerBase
+    {
+        private readonly Data.ILogActivityRepository c_logActivityRepository;
+
+
+
+        public LogActivityController(
+            [FromServices] Data.ILogActivityRepository logActivityRepository)
+        {
+            this.c_logActivityRepository = logActivityRepository;
+        }
+
+
+        [HttpPost]
+        public IActionResult Post(
+            [FromBody] Models.LogActivityRequest logActivityRequest)
+        {
+            var _requestId = Request.Headers["X-Request-Id"].ToString();
+            var _correlationId = Request.Headers["X-Correlation-Id"].ToString();
+
+            var _logActivity = new Entity.LogActivity(
+                Guid.Parse(_correlationId),
+                Guid.Parse(_requestId),
+                logActivityRequest.Service,
+                logActivityRequest.Activity,
+                logActivityRequest.ActivityDetail);
+
+            this.c_logActivityRepository.Log(_logActivity);
+
+            return this.StatusCode(StatusCodes.Status201Created);
+        }
+
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var _logActivityData = this.c_logActivityRepository.GetRecentLogs();
+            var _logActivityModel = _logActivityData.Select(
+                item => new LogActivity()
+                {
+                    Id = item.Id,
+                    CorrelationId = item.CorrelationId,
+                    RequestId = item.RequestId,
+                    Service = item.Service,
+                    Activity = item.Activity,
+                    ActivityDetail = item.ActivityDetail,
+                    Timestamp = item.Timestamp
+                });
+    
+            return this.StatusCode(StatusCodes.Status200OK, _logActivityModel);
+        }
+    }
+}
---
+++ reporting/Models/LogActivity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace reporting.Models
+{
+    public class LogActivity
+    {
+        public Guid Id;
+        public Guid CorrelationId;
+        public Guid RequestId;
+        public string Service;
+        public string Activity;
+        public string ActivityDetail;
+        public DateTime Timestamp;
+    }
+}
